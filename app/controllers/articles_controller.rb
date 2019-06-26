9df0e89aa1265dde1ff98c7fa3c349ad69b17721
Chris Allen <chrishughallen@gmail.com>
2019-06-26 11:48:21 +0200

    added edit feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 def new
 	@article= Article.new
 end
@@ -15,11 +17,38 @@ def create
 end
 
 def show
-	@article = Article.find(params[:id])
 end
+
+def update
+  if @article.update(article_params)
+   flash[:notice] = "Article was updated"
+   redirect_to article_path(@article)
+  else
+   flash[:notice] = "Article was not updated"
+   render 'edit'
+  end
+end
+
+def edit
+end
+
+def index
+  @articles = Article.all
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article was deleted"
+  redirect_to articles_path
+ end
 
 private 
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
 end
+
+def set_article
+   @article = Article.find(params[:id])
+  end
